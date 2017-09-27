7c30cd43e990238cad1cc63eb686fd8e622ef55c
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-09-27 16:26:27 -0500

    bytecounts command spiked in

--- cmd/bytecounts/main.go
+++ cmd/bytecounts/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
+	"log"
 	"os"
 
 	"github.com/pedmiston/bytecounter"
@@ -11,6 +13,18 @@ func main() {
 	query := flag.String("q", "", "term to search for on github.com")
 	limit := flag.Int("l", 10, "only get this many results")
 	flag.Parse()
-	repos := append(flag.Args(), bytecounter.Search(*query, *limit)...)
-	os.Stdout.Write(bytecounter.Totals(repos))
+	repos := flag.Args()
+	if *query != "" {
+		found, err := bytecounter.Search(*query, *limit)
+		if err != nil {
+			log.Fatal(err)
+		}
+		repos = append(repos, found...)
+	}
+	totals, err := bytecounter.GetTotals(repos)
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer := csv.NewWriter(os.Stdout)
+	writer.WriteAll(totals.ToCSV())
 }
--- totals.go
+++ totals.go
@@ -1,5 +1,13 @@
 package bytecounter
 
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/google/go-github/github"
+)
+
 // A Total contains the summary statistics for a repo.
 type Total struct {
 	Name string
@@ -14,7 +22,38 @@ type Total struct {
 	// PullRequests int
 }
 
-// Totals returns the summary statistics for each repo in repos.
-func Totals(repos []string) []Total {
-	return nil
+// Totals is a slice of Total objects.
+type Totals []*Total
+
+// GetTotals returns the summary statistics for each repo in repos.
+func GetTotals(repos []string) (Totals, error) {
+	client := github.NewClient(nil)
+	ctx := context.Background()
+
+	totals := make([]*Total, len(repos))
+	for i, fullName := range repos {
+		parts := strings.Split(fullName, "/")
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("unable to parse repo: %v", fullName)
+		}
+		owner, name := parts[0], parts[1]
+		repo, _, err := client.Repositories.Get(ctx, owner, name)
+		if err != nil {
+			return nil, fmt.Errorf("couldn't get repo %v: %v", fullName, err)
+		}
+		totals[i] = &Total{
+			Name: repo.GetFullName(),
+		}
+
+	}
+	return totals, nil
+}
+
+func (t Totals) ToCSV() [][]string {
+	records := make([][]string, len(t)+1)
+	records[0] = []string{"Name"}
+	for i, total := range t {
+		records[i+1] = []string{total.Name}
+	}
+	return records
 }
