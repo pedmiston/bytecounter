e4fadd89dc7a4e62a8f0725563678be33efce74a
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-09-27 23:10:19 -0500

    Spiked in serving up a js doc for D3

--- app/bytecountweb/main.go
+++ app/bytecountweb/main.go
@@ -8,6 +8,7 @@ func main() {
 	// Start a webserver to make the bytecounter package interactive
 	http.HandleFunc("/search", Search)
 	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
 }
 
